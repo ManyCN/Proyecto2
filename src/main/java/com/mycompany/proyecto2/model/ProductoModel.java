f9673709e26bbd0013663f94c4d23aa7d13732ae
ManyCN <manycolnav6937@gmail.com>
2023-06-29 08:59:00 -0600

    Implenta clases modelo

--- src/main/java/com/mycompany/proyecto2/model/ProductoModel.java
+++ src/main/java/com/mycompany/proyecto2/model/ProductoModel.java
@@ -9,5 +9,58 @@ package com.mycompany.proyecto2.model;
  * @author LENOVO
  */
 public class ProductoModel {
+    private String nombre;
+    private Double precio;
+    private Integer cantidad;
+    private String descripcion;
+
+    public ProductoModel() {
+        
+    }
+
+    public ProductoModel(String nombre, Double precio, Integer cantidad, String descripcion) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.descripcion = descripcion;
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductoModel{" + "nombre=" + nombre + ", precio=" + precio + ", cantidad=" + cantidad + ", descripcion=" + descripcion + '}';
+    }
     
 }
